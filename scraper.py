63de7f5f5f523afb442c432d84459dbda1cf7d5f
NeonLightning <neonlightning@gmail.com>
2024-06-16 11:33:14 -0400

    update

--- scraper.py
+++ scraper.py
@@ -1,11 +1,5 @@
-import discord
-import os
-import re
-import asyncio
-import argparse
+import discord, os, re, asyncio, argparse, random, aiohttp, logging
 from PIL import Image
-import random
-import aiohttp
 
 class MidjountySrefScraper(discord.Client):
     def __init__(self, *args, **kwargs):
@@ -22,9 +16,11 @@ class MidjountySrefScraper(discord.Client):
             if filename.startswith("sref_") and filename.endswith(".png"):
                 sref_number = filename.split("_")[1].split(".")[0]
                 self.downloaded_images.add(sref_number)
+        logging.info(f"downloaded {files}")
         print(f"downloaded {files}")
 
     async def on_ready(self):
+        logging.info(f'We have logged in as {self.user}')
         print(f'We have logged in as {self.user}')
         channel = self.get_channel(self.target_channel_id)
         async for message in channel.history(limit=None):
@@ -42,6 +38,7 @@ class MidjountySrefScraper(discord.Client):
             sref_number = self.get_sref_number(message.content)
             if sref_number and not self.is_image_downloaded(sref_number):
                 filename = os.path.join(self.save_path, f"sref_{sref_number}.png")
+                logging.info(f'saving download of {filename}')
                 print(f'saving download of {filename}')
                 async with aiohttp.ClientSession() as session:
                     for attachment in message.attachments:
@@ -56,13 +53,14 @@ class MidjountySrefScraper(discord.Client):
                                 image = Image.open(filename)
                                 resized_image = image.resize((int(image.width * 0.5), int(image.height * 0.5)))
                                 resized_image.save(filename)
-                                print(f"Image saved as {filename}")
+                                logging.info(f"Image saved as {filename}")
                                 self.downloaded_images.add(sref_number)
             elif self.is_image_downloaded(sref_number):
                 filename = os.path.join(self.save_path, f"sref_{sref_number}.png")
+                logging.debug(f'skipping download of {filename}')
                 print(f'skipping download of {filename}')
-        elif message.attachmens is not None and '"" --v 6.0 --ar 16:9 --sw 1000 --sref' not in message.content:
-            print('Attachment')
+        elif message.attachments is not None and '"" --v 6.0 --ar 16:9 --sw 1000 --sref' not in message.content:
+            logging.debug('Attachment')
 
     def get_sref_number(self, message_content):
         sref_index = message_content.find('--sref')
@@ -76,6 +74,7 @@ class MidjountySrefScraper(discord.Client):
         return sref_number in self.downloaded_images
 
     async def on_disconnect(self):
+        logging.debug("Disconnected from Discord. Attempting to reconnect...")
         print("Disconnected from Discord. Attempting to reconnect...")
 
     async def run_bot(self, token):
@@ -83,6 +82,7 @@ class MidjountySrefScraper(discord.Client):
             try:
                 await self.start(token)
             except (ConnectionResetError, discord.errors.HTTPException, aiohttp.ClientError) as e:
+                logging.error(f"An exception occurred: {e}. Reconnecting...")
                 print(f"An exception occurred: {e}. Reconnecting...")
                 await self.close()
                 await asyncio.sleep(5)
@@ -95,5 +95,16 @@ if __name__ == "__main__":
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     client = MidjountySrefScraper(save_path=save_path)
+    try:
+        print(f"Log file path: {os.path.abspath('scraper.log')}")
+        logging.debug(f"Log file path: {os.path.abspath('scraper.log')}")
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        logging.basicConfig(filename='/home/neonbot/midjourney/scraper.log', level=logging.DEBUG)
+        logger = logging.getLogger()
+        handler = logger.handlers[0]
+        handler.setFormatter(formatter)
+    except Exception as e:
+        print(f"Error setting up logging: {e}")
+        logging.error(f"Error setting up logging: {e}")
     token = 'CLIENT_ID_HERE'
     asyncio.run(client.run_bot(token))
